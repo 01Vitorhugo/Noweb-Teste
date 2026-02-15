7680655cd70513d2d09a23f1563d207ddce82f47
Vitor <vitorhugo0031@outlook.com>
2026-02-14 21:34:57 -0300

    feat: adicioando campos para editar

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -10,12 +10,30 @@
         
     <form action="{{ route('posts.update', $post) }}" method="POST">
         @csrf
-        @method('PUT') {{-- OBRIGATÓRIO PARA UPDATE --}}
+        @method('PUT') 
+
+        <label class="block text-gray-700">Título</label>
+        <input type="text" name="title" value="{{ $post->title }}" class="border p-2 w-full  ">
+
+        <label class="block text-gray-700">Categoria</label>
+                    <select name="category_id" class="w-full border p-2 rounded">
+                        @foreach($categories as $category)
+                            <option value="{{ $category->id }}" {{ $post->category_id == $category->id ? 'selected' : '' }}>{{ $category->name }}</option>
+                        @endforeach
+                    </select>
+
+        <label class="block text-gray-700">Tag</label>
+        <input type="text" name="tag" value="{{ $post->tag }}" class="border p-2 w-full">
+
+        <label class="block text-gray-700">Resumo</label>
+        <input type="text" name="summary" value="{{ $post->summary }}" class="border p-2 w-full">
+
+        <label class="block text-gray-700">Conteúdo Completo</label>
+        <textarea name="content" class="w-full border p-2 rounded" rows="5">{{ $post->content }}</textarea>
         
-        <input type="text" name="title" value="{{ $post->title }}" class="border p-2 w-full">
-        {{-- Repita os outros campos com value="{{ $post->campo }}" --}}
         
         <button type="submit" class="bg-blue-500 text-white p-2">Atualizar</button>
+        <a href="{{ route('posts.index') }}" class="ml-4 text-gray-500">Cancelar</a>
     </form>
 
     </div>
